f8f077c438575922932d68f903531b06243b9117
Sibin Thomas <sibintho007@gmail.com>
2019-03-07 03:37:15 +0530

    firstc

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended:true}));
 
-mongoose.connect('mongodb+srv://sibin:st654321@cluster0-n5hf2.mongodb.net/test?retryWrites=true/bookmarkdb',(err)=>{
+mongoose.connect('mongodb+srv://sibin:st654321@cluster0-n5hf2.mongodb.net/bookmarkdb?retryWrites=true',(err)=>{
 	if (err) throw err;
 	console.log('connection established');
 });
