a81404892410df5f941c3284f55e32c49822b4f6
Sibin Thomas <sibintho007@gmail.com>
2019-03-07 02:31:00 +0530

    firstc

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended:true}));
 
-mongoose.connect('mongodb+srv://thomas:<password>@cluster0-n5hf2.mongodb.net/test?retryWrites=true/bookmarkdb');
+mongoose.connect('mongodb+srv://sibin:st654321@cluster0-n5hf2.mongodb.net/test?retryWrites=true/bookmarkdb');
 app.get('/',(req,res)=>{
 	res.send('enter');
 })
